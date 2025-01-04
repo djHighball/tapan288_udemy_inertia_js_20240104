90951585e925f665d62e2ec0a178a7f4b9bcad4e
k.adachi <adachi_keita2@nexcert.com>
2025-01-04 17:13:32 +0900

    add: #8 フォーム作成まで

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,9 @@ class PostController extends Controller
             'posts' => PostResource::collection($posts),
         ]);
     }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
 }
--- resources/js/Pages/Posts/Index.tsx
+++ resources/js/Pages/Posts/Index.tsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, useForm } from '@inertiajs/react';
 
 // コントローラから受け取るデータの型を定義
 interface User {
@@ -21,8 +21,21 @@ interface PostsProps {
     }
 }
 
+
 export default function Dashboard({posts}: PostsProps) {
     console.log(posts);
+ 
+    // InertiaのFormヘルパを使用。下記の「Form Helper」を参照
+    // https://inertiajs.com/forms
+    const {data, setData, post, processing, errors} = useForm({
+        body: '',
+    });
+
+    function submit(e) {
+        e.preventDefault();
+        post(route('posts.store'));
+    }
+
     return (
         <AuthenticatedLayout
             header={
@@ -36,12 +49,34 @@ export default function Dashboard({posts}: PostsProps) {
             </Head>
 
             <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-3xl sm:px-6 lg:px-8 space-y-3">
+                    <form onSubmit={submit} className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <label htmlFor="body" className="sr-only">Body</label>
+                        <textarea
+                            onChange={(e) => setData('body', e.target.value)}
+                            name="body"
+                            id="body"
+                            cols="30"
+                            rows="5"
+                            className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
+                        ></textarea>
+                        <button
+                            type="submit"
+                            className="mt-2 bg-gray-700 px-4 py-2 rounded-md font-medium text-white"
+                        >
+                            Post
+                        </button>
+                    </form>
+
+
                     {posts.data.map((post) => {
                         return (
-                            <div key={post.id}>
+                            <div key={post.id} className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                                <div className="p-6 text-gray-900">
                                     <div className="font-semibold">{post.user.name}</div>
-                                    <div className="mt-1">{post.body}</div>
+                                    <p className="mt-1">{post.body}</p>
+
+                                </div>
                             </div>
                         )
                     })}
